5f8efba628360c48851530291465d740f08f55bd
Daniel <unixdude@mac.com>
2021-09-13 22:42:17 -0400

    Initial checkin

---
+++ roles/docker-container-scanner/files/scanner.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -x
+
+docker container run \
+    -v /auto/documents/Scans/Docker:/app/data/output \
+    -v /var/run/dbus:/var/run/dbus \
+    -u 0 \
+    --net host \
+    --privileged \
+    --name scanner \
+    --restart unless-stopped \
+    -p 8080:8080 \
+    -d \
+    eagle243/scanservjs
